f45a725d0fe87b0f1dea8d86d5f188d51ded27b5
handbellmin <whdals9265@naver.com>
2023-02-07 14:55:02 +0900

    커밋 테스트

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    @GetMapping("/")
+    @GetMapping("")
     public void main(){
         System.out.println("Test Connect docker-jenkinks");
 
